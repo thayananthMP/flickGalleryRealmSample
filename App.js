94a45c9185f7d89aecaa5f8b305eb2fed543ec41
thayananthMP <mpthayananth97@gmail.com>
2022-12-09 15:20:51 +0530

    Images from Flickr API displayed

--- App.js
+++ App.js
@@ -1,39 +1,79 @@
 import React, {Component} from 'react';
-import {Stylesheet, Image, SafeAreaView, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Dimensions,
+  Image,
+  SafeAreaView,
+  Text,
+  View,
+  FlatList,
+} from 'react-native';
 import axios from 'axios';
 
-const getFlickrImageURL = (photo, size) => {
-  let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`;
-  if (size) {
-    // Configure image size
-    url += `_${size}`;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {imageURLs: [], imageTitle: []};
   }
-  url += '.jpg';
-  console.log(url);
-  return url;
-};
 
-const getImageURL = () => {
-  axios
-    .get(
-      'https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1',
-    )
-    .then(response => response.json())
-    .then(data =>
-      // get an array of image-url
-      data.photos.photo.map(photo => {
-        return getFlickrImageURL(photo, 'q');
-      }),
+  componentDidMount() {
+    //For our first load.
+    this.getImageURL();
+  }
+
+  getImageURL() {
+    axios
+      .get(
+        'https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1',
+      )
+      .then(data =>
+        // get an array of image-url
+        data.data.photos.photo.map(photo => {
+          return this.getFlickrImageURL(photo);
+        }),
+      )
+      .catch(error => console.log(error));
+    return;
+  }
+  getFlickrImageURL(photo) {
+    let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`;
+    url += '.jpg';
+    this.setState({imageURLs: [...this.state.imageURLs, url]});
+    this.setState({imageTitle: [...this.state.imageTitle, photo.title]});
+    return url;
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <FlatList
+            style={styles.flatListStyle}
+            data={this.state.imageURLs}
+            numColumns={2}
+            renderItem={({item}) => {
+              return (
+                <View>
+                  <Image style={styles.image} source={{uri: item}} />
+                </View>
+              );
+            }}
+          />
+        </View>
+      </SafeAreaView>
     );
-  return;
-};
+  }
+}
 
-const App = () => {
-  return (
-    <SafeAreaView>
-      <View>{getImageURL()}</View>
-    </SafeAreaView>
-  );
-};
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F3F3',
+  },
+  image: {
+    width: Dimensions.get('window').width / 2 - 20,
+    height: 150,
+    margin: 10,
+  },
+  flatListStyle: {flex: 1},
+});
