3c11d2ad5f9a8676133bd1ace9482382b7676646
thayananthMP <mpthayananth97@gmail.com>
2022-12-22 13:43:43 +0530

    Flickr API and Realm storage added

--- App.js
+++ App.js
@@ -4,83 +4,106 @@ import {
   Dimensions,
   Image,
   SafeAreaView,
+  Text,
   RefreshControl,
-  ScrollView,
   View,
   FlatList,
 } from 'react-native';
 import axios from 'axios';
-import Realm, {Object} from 'realm';
+import Realm from 'realm';
 
-let taskName;
+let data = [];
 let realm;
 
 const wait = timeout => {
   return new Promise(resolve => setTimeout(resolve, timeout));
 };
 
+// Realm Schema
 const TaskSchema = {
   name: 'Task',
   properties: {
-    // _id: 'int',
     name: 'string',
+    url: 'string',
     status: 'string?',
   },
-  // primaryKey: '_id',
 };
 
-addToRealm = value => {
+// Add values to Realm database
+addToRealm = (url, title) => {
   (async () => {
     try {
       realm = await Realm.open({
         schema: [TaskSchema],
         deleteRealmIfMigrationNeeded: true,
       });
-      this.setState({realmObject: realm});
 
-      await realm.write(() => {
+      // Inserting values into Realm database
+      realm.write(() => {
         taskName = realm.create('Task', {
-          name: value,
+          name: title,
+          url: url,
           status: 'Open',
         });
       });
     } catch (e) {
-      console.log('error', e.message);
+      console.log('Error', e.message);
     }
   })();
 };
+
+// Fetch values from Realm database
+getRealmValues = () => {
+  (async () => {
+    try {
+      realm = await Realm.open({
+        schema: [TaskSchema],
+        deleteRealmIfMigrationNeeded: true,
+      });
+
+      // Getting values from Realm object
+      const tasks = realm.objects('Task');
+      tasks.map(task => (data = [...data, task]));
+    } catch (e) {
+      console.log('Error', e.message);
+    }
+  })();
+};
+
+// Image rendering element
+function Item({item}) {
+  return (
+    <View>
+      <Image style={styles.image} source={{uri: item.url}} />
+      <Text style={{width: 170, textAlign: 'center', justifyContent: 'center'}}>
+        {item.name}
+      </Text>
+    </View>
+  );
+}
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       refreshing: false,
-      realmObject: Object,
-      imageName: '',
-      imageURLs: [],
-      imageTitle: [],
-      ImageBackupItems: [],
+      images: [],
     };
   }
 
   componentDidMount() {
-    //For our first load.
+    //On first load.
     this.getImageURL();
   }
 
-  getRealmValues() {
-    console.log('inside method', this.state.realmObject);
-    // const tasks = this.state.realmObject.objects('Task');
-    // console.log(`The lists of tasks are: ${tasks.map(task => task.name)}`);
-  }
-
+  //Fetching image-url from Flickr API
   getImageURL() {
     axios
       .get(
         'https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1',
       )
       .then(data =>
-        // get an array of image-url
+        // Get an array of image-url
         data.data.photos.photo.map(photo => {
           return this.getFlickrImageURL(photo);
         }),
@@ -89,22 +112,27 @@ export default class App extends Component {
     return;
   }
 
+  //Modifying  image-url
   getFlickrImageURL(photo) {
     let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}`;
     url += '.jpg';
-    this.setState({imageURLs: [...this.state.imageURLs, url]});
-    this.setState({imageTitle: [...this.state.imageTitle, photo.title]});
+
+    //Add to Realm database
+    addToRealm(url, photo.title);
+
+    this.setState({
+      images: [...this.state.images, {url: url, name: photo.title}],
+    });
     return url;
   }
 
   onRefresh() {
-    this.state.refreshing = true;
-    console.log('images in array', this.state.imageURLs);
-    this.state.imageURLs = [];
-    this.getRealmValues();
-    console.log('images in array', this.state.imageURLs);
-    wait(2000).then(() => {
-      this.state.refreshing = false;
+    this.setState({images: []});
+    // Getting values from Realm database
+    getRealmValues();
+    this.setState({refreshing: true});
+    wait(1000).then(() => {
+      this.setState({refreshing: false});
     });
   }
 
@@ -121,16 +149,9 @@ export default class App extends Component {
               />
             }
             style={styles.flatListStyle}
-            data={this.state.imageURLs}
-            // extraData={}
+            data={this.state.images.length > 0 ? this.state.images : data}
             numColumns={2}
-            renderItem={({item}) => {
-              return (
-                <View>
-                  <Image style={styles.image} source={{uri: item}} />
-                </View>
-              );
-            }}
+            renderItem={({item}) => <Item item={item} />}
           />
         </View>
       </SafeAreaView>
